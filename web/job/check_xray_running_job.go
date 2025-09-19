6b78944bad6ac725982ff07fe026bf331189d1b9
alisonsimpson61-png <alisonsimpson61@gmail.com>
2025-09-19 14:16:53 +0800

    Initial commit - clean repo

---
+++ web/job/check_xray_running_job.go
@@ -0,0 +1,32 @@
+package job
+
+import (
+	"github.com/MHSanaei/3x-ui/logger"
+	"github.com/MHSanaei/3x-ui/web/service"
+)
+
+type CheckXrayRunningJob struct {
+	xrayService service.XrayService
+
+	checkTime int
+}
+
+func NewCheckXrayRunningJob() *CheckXrayRunningJob {
+	return new(CheckXrayRunningJob)
+}
+
+func (j *CheckXrayRunningJob) Run() {
+	if !j.xrayService.DidXrayCrash() {
+		j.checkTime = 0
+	} else {
+		j.checkTime++
+		// only restart if it's down 2 times in a row
+		if j.checkTime > 1 {
+			err := j.xrayService.RestartXray(false)
+			j.checkTime = 0
+			if err != nil {
+				logger.Error("Restart xray failed:", err)
+			}
+		}
+	}
+}
